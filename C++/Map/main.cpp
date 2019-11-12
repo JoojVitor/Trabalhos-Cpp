55729690f9c812a11b668116afab48d88cb6a8b6
João Vitor <54105984+JoojVitor@users.noreply.github.com>
2019-11-12 14:58:40 -0300

    *não finalizado*

--- C++/Map/main.cpp
+++ C++/Map/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "mapavl.cpp"
+using namespace std;
 
+int main()
+{
+    MapAVL im;
+    thing a,aux;
+
+    for (int i=0; i<10; i++) {
+        a.key = i;
+        im.Push(&a);
+    }
+    im.Remove(&im.root,8,&aux);
+    //cout<< aux.key <<endl;
+}
