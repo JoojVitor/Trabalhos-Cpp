ce26c04000cc0057ba34020012690cf5f39fb56c
João Vitor <54105984+JoojVitor@users.noreply.github.com>
2019-11-13 14:57:53 -0300

    Update list.h

--- C++/List/list.h
+++ C++/List/list.h
@@ -2,18 +2,19 @@
 #define LIST_H
 #include "node.h"
 
+template<class thing>
 class List
 {
 private:
-    Node *Head;
-    Node *Tail;
+    Node<thing> *Head;
+    Node<thing> *Tail;
 
 public:
     List();
     bool IsEmpty(){return !Head;}
-    bool Push(int *DAT);
-    bool Pop(int K, int *DAT);
-    bool search(int K, int *DAT);
+    bool Push(thing *DAT);
+    bool Pop(int K, thing *DAT);
+    bool search(int K, thing *DAT);
 };
 
 #endif // LIST_H
