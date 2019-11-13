1cb851c4119a24c230d87abe66e1fcb659bacc5f
João Vitor <54105984+JoojVitor@users.noreply.github.com>
2019-11-13 14:53:01 -0300

    Update dclist.cpp

--- C++/List (Circular Doubly Linked)/dclist.cpp
+++ C++/List (Circular Doubly Linked)/dclist.cpp
@@ -9,48 +9,56 @@ DCList<thing>::DCList()
 
 template <class thing>
 bool DCList<thing>::Push(thing *DAT){
-    Node<thing>* P= Node<thing>::montaNode(DAT);
-    if(P){
-        if(!head){
-            head = P->next = P->prev = P;
-         }else{
-            P->next = head;
-            P->prev = head->prev;
-            head->prev = P;
-            head = P;
-         }
-         size++;
-         return true;
+    Node<thing> *P = Node<thing>::montaNode(DAT);
+    if(!P)
+        return false;
+    if(!head){
+        head = P->next = P->prev = P;
+    }else{
+        P->next = head;
+        P->prev = head->prev;
+        (P->prev)->next = P;
+        head->prev = P;
+        head = P;
+    }
+    return true;
+}
+
+template <class thing>
+bool DCList<thing>::Search(thing *DAT, int K){
+    if(!head) return false;
+        Node<thing>* P = head;
+    while(P->next!=head and (P->D) != K){
+        P = P->next;
+    }
+    if((P->D)==K){
+        *DAT = P->D;
+        return true;
     }
     return false;
 }
 
 template <class thing>
 bool DCList<thing>::Pop(thing *DAT, int K){
-    if(!head) return false;
-            Node<thing>* P = head;
-            while(P->next!=head and (P->D) != K)
-            {
-                P = P->next;
+    if(!head)
+        return false;
+    Node <thing> *P = head;
+
+    while(P->next!=head and P->D!=K){
+        P = P->next;
+    }
+    if(P->D == K){
+        if(P->next == (P->next)->prev){
+            head = nullptr;
+        }else{
+            (P->prev)->next = P->next;
+            (P->next)->prev = P->prev;
+            if(head == P){
+                head = P->next;
             }
-            if((P->D)==K)
-            {
-                if(P->next==P->prev)
-                {
-                    head = nullptr;
-                }
-                else
-                {
-                    (P->next)->prev=(P->prev);
-                    (P->prev)->next=(P->next);
-                    if(head == P)
-                    {
-                        head = P->next;
-                    }
-                }
-                Node<thing>::desmontaNode(P,DAT);
-                size--;
-                return true;
-            }
-            return false;
         }
+        Node<thing>::desmontaNode(P,DAT);
+        return true;
+    }
+    return false;
+}
