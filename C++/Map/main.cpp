106c9bf33dc25b1035f5b95028b217155c4335b4
João Vitor <54105984+JoojVitor@users.noreply.github.com>
2019-12-11 20:40:33 -0300

    Update main.cpp

--- C++/Map/main.cpp
+++ C++/Map/main.cpp
@@ -2,15 +2,11 @@
 #include "mapavl.cpp"
 using namespace std;
 
-int main()
-{
-    MapAVL im;
-    thing a,aux;
+int main(){
 
-    for (int i=0; i<10; i++) {
-        a.key = i;
-        im.Push(&a);
-    }
-    im.Remove(&im.root,8,&aux);
-    //cout<< aux.key <<endl;
+    MapAVL<int> M;
+    Thing<int> aux;
+    aux.key = 1;
+    M.Push(&aux);
+
 }
