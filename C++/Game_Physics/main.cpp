8d9e136740bda41b979e49b16602de4741c59de9
João Vitor <54105984+JoojVitor@users.noreply.github.com>
2019-12-17 15:05:10 -0300

    Update main.cpp

--- C++/Game_Physics/main.cpp
+++ C++/Game_Physics/main.cpp
@@ -1,28 +1,68 @@
 #include <iostream>
-#include "mapavl.h"
-#include "jogador.h"
+#include <locale.h>
+#include "mapavl.cpp"
+#include "entity.h"
 using namespace std;
 
 bool control(){
-
+    return true;
 }
 
 int main(){
-    MapAVL<jogador> data;
+    setlocale(LC_ALL, "Portuguese");
+
+    MapAVL data;
+    pair<pos,entity> x, aux;
+    pos p;
     int N;
-    cout<< "Digite o numero de jogadores: ";
+
+    cout<< "Digite o número de jogadores: ";
     cin>> N;
+
     //system("clear");
     system("cls");
-    jogador P[N+1];
+
+    entity E[N+1];
     int l,c,h,d;
+
     for(int i=1; i<=N; i++){
-        cout<< "Digite posição inicial do jogador " << i << " :";
+        E[i].setName("Jogador " + to_string(i));
+        E[i].setTipo("Jogador");
+        cout<< "Digite posicao inicial do jogador " << i << " :";
         cin>> l >> c;
-        P[i].setPos(l,c);
-        cout<< "Digite o HP e Dano: ";
-        cin>> h >> d;
-        P[i].setHp(h);
-        P[i].setDano(d);
+        p.setPosition(l,c);
+        E[i].setPos(l,c);
+        cout<< "Digite o HP: ";
+        cin>> h;
+        E[i].setHp(h);
+        cout<< "Digite o Dano: ";
+        cin >> d;
+        E[i].setDano(d);
+
+        x.first = p;
+        x.second = E[i];
+        data.Push(&x);
+    }
+
+    int auxl, auxc;
+    cout<< "Digite os limites do mapa: ";
+    cin>> auxl >> auxc;
+    entity mapa[auxl][auxc];
+
+    for(int i=0; i<=N; i++){
+        E[i].getPos(&l,&c);
+        mapa[l][c] = E[i];
+    }
+
+    for(int i=0; i<auxl; i++){
+        for(int j=0; j<auxc; j++){
+            if(mapa[i][j].getTipo() == "Jogador"){
+                cout<< 'X';
+            }else{
+                cout<< '-';
+            }
+            cout<< "\t";
+        }
+        cout<< "\n";
     }
 }
